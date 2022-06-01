bb5deb1300acceff7bb1dde860780c6aacfd49ba
AlexandraHessen <xl_energy@mail.ru>
2022-06-01 13:54:11 +0300

    @NgModule

--- A_Ang0/Ang010_hello/src/app/app.module.ts
+++ A_Ang0/Ang010_hello/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [
+  imports: [ BrowserModule ], //какие другие модули этому модулю нужны
+  declarations: [ //какие компоненты будут описаны в этом модуле (из чего состоит)
     HelloComponent
   ],
   providers: [],
-  bootstrap: [HelloComponent]
+  bootstrap: [HelloComponent] // с какого компонента начать все рендер (типо App.js)
 })
 export class AppModule { }
