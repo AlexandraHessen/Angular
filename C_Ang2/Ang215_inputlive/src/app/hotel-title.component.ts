b142f1c60266491f03ee13f8632682c03abc1922
AlexandraHessen <xl_energy@mail.ru>
2022-06-02 22:32:56 +0300

    ngOnChanges add comments

--- C_Ang2/Ang215_inputlive/src/app/hotel-title.component.ts
+++ C_Ang2/Ang215_inputlive/src/app/hotel-title.component.ts
@@ -11,14 +11,14 @@ export class HotelTitleComponent implements OnChanges {
   @Input("hotel-name")
   private hotelName:string;
 
-  ngOnChanges(changes: {[property: string]: SimpleChange }) { 
-    // получим изменения только свойства hotelName,
-    // связанного с входным свойством
+  ngOnChanges(changes: {[property: string]: SimpleChange }) {
+    // changes - объект в котором указано что поменялось
     let change:SimpleChange=changes["hotelName"];
-    // SimpleChange содержит:                                        
-    // currentValue - текущее значениесвойства
+    // changes["hotelName"] - из всех изменений changes мы берет только  изменения атрибута hotelName
+
     console.log('атрибут hotel-name получил новое значение: '
       +change.currentValue);
+      // у change есть много других свойств
   }
 
   getName():string {
@@ -26,3 +26,20 @@ export class HotelTitleComponent implements OnChanges {
   }
 
 }
+
+import { Output, EventEmitter } from '@angular/core';
+
+@Output("over-speed")
+private overSpeed:EventEmitter<string>=new EventEmitter<string>();
+//выходное свойство overSpeed типа EventEmitter<string>, т.е. это будет поток строк, можно и других
+
+...и в какой-то момент:
+this.overSpeed.emit("speed is very high!");
+
+а в родительском-компоненте:
+
+<car (over-speed)="alert($event)">
+// car - родительский компонент
+// (over-speed) - подписывается на событие
+// "alert($event)" - что надо сделать по событию
+// $event - объект события = "speed is very high!"
