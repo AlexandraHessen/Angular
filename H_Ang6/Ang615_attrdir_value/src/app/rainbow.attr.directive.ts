ffcdd7db97fa8ce659475c3801fc12b47a1ce358
AlexandraHessen <xl_energy@mail.ru>
2022-06-03 23:59:12 +0300

    Directive

--- H_Ang6/Ang615_attrdir_value/src/app/rainbow.attr.directive.ts
+++ H_Ang6/Ang615_attrdir_value/src/app/rainbow.attr.directive.ts
@@ -1,9 +1,9 @@
-﻿import { Directive, ElementRef, Input, AfterViewInit } from "@angular/core"; 
+﻿import { Directive, ElementRef, Input, AfterViewInit } from "@angular/core";
 
-@Directive({ 
-  selector: "[rainbow]", 
-}) 
-export class RainbowDirective implements AfterViewInit { 
+@Directive({
+  selector: "[rainbow]",
+})
+export class RainbowDirective implements AfterViewInit {
 
   colors:Array<string>
     =['red','green','blue','cyan','magenta','yellow'];
@@ -15,13 +15,13 @@ export class RainbowDirective implements AfterViewInit {
 
     // к этому моменту startColor ещё не готов
 
-    setInterval( ()=>{ 
-      this.setRandomColor() 
+    setInterval( ()=>{
+      this.setRandomColor()
     },2000);
-  } 
+  }
 
   ngAfterViewInit():void {
-    
+
     // к этому моменту startColor уже готов
 
     this.element.nativeElement
@@ -40,3 +40,63 @@ export class RainbowDirective implements AfterViewInit {
   }
 
 }
+
+import { Directive, HostBinding } from "@angular/core";
+
+@Directive({
+  selector: "[атрибут]",
+})
+class имяклассадирективы {
+
+  @HostBinding("атрибут2")
+  доступ переменная:тип;
+
+    ...или...
+
+  @HostBinding("атрибут2")
+  get геттер() { . . . }
+
+}
+
+import { Directive, HostListener } from "@angular/core";
+
+@Directive({
+  selector: "[атрибут]",
+})
+class имяклассадирективы {
+
+  @HostListener("имясобытия")
+  доступ метод():тип {
+    . . .
+  }
+
+или, если методу нужно передать аргументы (чаще всего $event события):
+
+  @HostListener("имясобытия",[строки-выражения])
+  доступ метод(аргументы):тип {
+    . . .
+  }
+
+}
+
+@HostListener("имясобытия",[строки-выражения])
+доступ метод(аргументы):тип {
+  . . .
+}
+
+}
+
+import { Directive, HostListener } from "@angular/core";
+
+@Directive({
+  selector: "[myattr]",
+})-
+class MyAttrDirective {
+
+  @HostListener("mouseover",["$event"])
+  private myClicked(ev:Event):void {
+    console.log(ev.target);
+  }
+
+}
+
